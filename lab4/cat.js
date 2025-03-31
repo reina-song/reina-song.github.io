6087e2432da48e61e60cc97da1bc86b764850516
Reina-Song <81272042+Reina-Song@users.noreply.github.com>
2025-03-31 02:44:55 -0400

    Update cat.js

--- lab4/cat.js
+++ lab4/cat.js
@@ -12,6 +12,25 @@ function setup() {
     image(kitty, 300 , 550, 100, 100);
     image(kitty, 400 , 550, 100, 100);
   
+
+      // Create a p5.Image object.
+  let img = createImage(100, 100);
+  img.loadPixels();
+  // Create a color gradient.
+  for (let x = 0; x < img.width; x += 1) {
+    for (let y = 0; y < img.height; y += 1) {
+      // Calculate the transparency.
+      let a = map(x, 0, img.width, 0, 255);
+      // Create a p5.Color object.
+      let c = color(255, 0, 100, a);
+      // Set the pixel's color.
+      img.set(x, y, c);
+    }
+  }
+    img.updatePixels();
+    image(img, 500, 550);
+  
+
     let video = createVideo('/assets/yakki.mov');
     video.size(windowWidth/2, windowHeight/2);
     video.position(20, 10); 
